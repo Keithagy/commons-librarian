4aa226a5a3dc099aaa48294188d4c2b7169650a7
Keith Ang <keithang@keiths-mbp.home>
2024-06-20 12:31:06 +0100

    feat(determineIfEntityTypePresent): implement entity detection step of pipeline

--- src/workflow/determineIfEntityTypePresent.ts
+++ src/workflow/determineIfEntityTypePresent.ts
@@ -1,10 +1,68 @@
-import { NotImplementError } from "../errors";
+import { llmCompletion } from "src/llm/completion";
 import { EntityDefinition } from "../schema/entity";
+import { BadLLMResponse } from "../errors";
 import { VaultPage } from "obsidian-vault-parser";
+import { printNode, zodToTs } from "zod-to-ts";
+import { z } from "zod";
 
 export async function determineIfEntityTypePresent(
   file: VaultPage,
-  entDef: EntityDefinition,
+  entityDefinition: EntityDefinition,
 ): Promise<boolean> {
-  throw new NotImplementError();
+  const contents = file.content;
+  if (contents === null) {
+    return false; // file contents empty; couldn't possibly contain entity
+  }
+  // TODO: chunking consideration
+
+  // TODO: determine parsing logic
+  const zod_scalar_parser = z.object({});
+  const tsSchema = printNode(zodToTs(zod_scalar_parser).node);
+  const rest = await llmCompletion({
+    messages: [
+      {
+        role: "system",
+        content: `
+Your role is to determine if the file content below describes at least one instance of the following entity called ${entityDefinition.name}, represented by the following schema:
+\`\`\`ts
+${tsSchema}
+\`\`\`
+
+File content:
+\`\`\`
+${file.content}
+\`\`\`
+
+You are to respond ONLY with one of the following values:
+- \`true\` if the file describes at least one instance of entity ${entityDefinition.name}
+- \`false\` if the file describes no such entity
+- \`null\` if unclear`,
+      },
+    ],
+  });
+
+  const response_json_text = rest.choices[0].message.content;
+  if (!response_json_text) {
+    throw new BadLLMResponse(rest.choices[0].finish_reason);
+  }
+
+  const response: boolean | null = JSON.parse(response_json_text);
+
+  // TODO: this response indicates that the LLM was unsure; needs a retry mechanism
+  if (response === null) {
+    throw new BadLLMResponse(
+      `LLM could not determine if entity ${entityDefinition.name} was present in the file at ${file.path}`,
+    );
+  }
+  if (typeof response !== "boolean") {
+    throw new BadLLMResponse(
+      `LLM did not provide a valid (boolean) response when checking if entity ${entityDefinition.name} was present in the file at ${file.path}`,
+    );
+  }
+  console.info(
+    `Entity ${entityDefinition.name} ${
+      (response && "exists") || "does not exist"
+    } in document at ${file.path}`,
+  );
+  return response;
 }

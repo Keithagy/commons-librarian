90c48de1781292f26f674f6d56fb07d3711b5c47
Keith Ang <keithang@keiths-mbp.home>
2024-06-20 20:31:11 +0100

    feat(parse-existing-vault): implement parsing into knowledge graph from existing vault, scalar values

--- src/schema/entity.ts
+++ src/schema/entity.ts
@@ -1,7 +1,10 @@
 import z from "zod";
 import { zConstraintDefinition } from "./constraints";
-import { FieldInstance, zFieldDefinition } from "./field";
+import { FieldDefinition, FieldInstance, zFieldDefinition } from "./field";
 import { IsAny } from "../helpers/utility-types";
+import { EntityNotFoundError, NotImplementError } from "src/errors";
+import { VaultPage } from "obsidian-vault-parser";
+import { printNode, zodToTs } from "zod-to-ts";
 
 export const zEntityName = z
   .string()
@@ -25,3 +28,51 @@ export type EntityInstance<T extends EntityDefinition> = {
 } & {
   __type: IsAny<T["name"]> extends true ? string : NonNullable<T["name"]>;
 };
+
+export type EntityInstanceType = EntityInstance<EntityDefinition>["__type"];
+
+export function retrieveEntityDefinition(
+  entityTypeName: EntityInstanceType,
+  entityDefinitionsByName: Record<EntityInstanceType, EntityDefinition>,
+): EntityDefinition {
+  const maybeEntityDefinition = entityDefinitionsByName[entityTypeName];
+  if (!maybeEntityDefinition) {
+    throw new EntityNotFoundError(entityTypeName);
+  }
+  return maybeEntityDefinition;
+}
+
+export function parseEntity<T extends EntityDefinition>(
+  file: VaultPage,
+  expected: T,
+): EntityInstance<T> {
+  const scalarFields = expected.fields.filter(
+    (f) => f.type === "scalar",
+  ) as Extract<FieldDefinition, { type: "scalar" }>[];
+  let zod_scalar_parser = z.object({});
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let zfield: z.ZodType<any, any, any>;
+  scalarFields.forEach((field) => {
+    switch (field.value) {
+      case "string":
+        zfield = z.string();
+        break;
+      case "number":
+        zfield = z.number();
+        break;
+      case "boolean":
+        zfield = z.boolean();
+        break;
+      default:
+        throw new NotImplementError(
+          `Field value ${field.value} not implemented`,
+        );
+    }
+    zod_scalar_parser = zod_scalar_parser.extend({
+      [field.name]: zfield,
+    });
+  });
+  const result = zod_scalar_parser.parse(file.frontMatter) as EntityInstance<T>;
+  console.log("Parsed data:", result);
+  return result;
+}
--- src/workflow/parseExistingGlobalGraphFromVault.ts
+++ src/workflow/parseExistingGlobalGraphFromVault.ts
@@ -1,9 +1,28 @@
-import { NotImplementError } from "../errors";
 import { Vault } from "obsidian-vault-parser";
 import { KnowledgeGraph } from "./types";
+import {
+  EntityDefinition,
+  EntityInstanceType,
+  parseEntity,
+  retrieveEntityDefinition,
+} from "src/schema/entity";
 
+export const ENTITY_TYPE_FRONTMATTER_KEY = "__type";
 export function parseExistingGlobalGraphFromVault(
   existingVault: Vault,
+  entityDefinitionsByName: Record<EntityInstanceType, EntityDefinition>,
 ): KnowledgeGraph {
-  throw new NotImplementError();
+  const knowledgeGraph: KnowledgeGraph = new Set();
+
+  for (const file of Object.values(existingVault.files)) {
+    const entityType: EntityInstanceType =
+      file.frontMatter[ENTITY_TYPE_FRONTMATTER_KEY];
+    const entityDefinition = retrieveEntityDefinition(
+      entityType,
+      entityDefinitionsByName,
+    );
+    const entity = parseEntity(file, entityDefinition);
+    knowledgeGraph.add(entity);
+  }
+  return knowledgeGraph;
 }

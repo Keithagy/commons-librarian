d18cc1f82c8ed928587a5d90daeb8bb2e051f77f
David Rohmer <rohmer.david@gmail.com>
2024-06-23 00:30:36 +0100

    prototype second schema

---
+++ src/use-case/2-regen-schema.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import { EntityDefinition, EntityInstance } from "../schema/entity";
+
+export const personEntity = {
+  type: "entity",
+  name: "Person",
+  fields: [
+    {
+      name: "full_name",
+      type: "scalar",
+      value: "string",
+    },
+    {
+      name: "age",
+      type: "scalar",
+      value: "number",
+      comment: "This person's age in years."
+    },
+    {
+      type: "link",
+      name: "parent",
+      target: "Person",
+      multi: false,
+    },
+    {
+      type: "zod",
+      name: 'urls',
+      value: z.array(z.string()),
+    }
+  ],
+  constraints: [
+    {
+      type: "primary-key",
+      field: "full_name",
+    },
+  ],
+} satisfies EntityDefinition;
+
+const schema = [
+  personEntity
+]
+
+export default schema;
